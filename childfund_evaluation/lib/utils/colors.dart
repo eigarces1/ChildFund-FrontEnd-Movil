a42660721f2f8767a0870dc74cd803ea9bee7ffa
AndresLopez13 <andresilopeza@gmail.com>
2024-01-22 12:06:24 -0500

    add widget ChildEvaluationForm and Colors

---
+++ childfund_evaluation/lib/utils/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xFF7ab800);
+  //static const Color secundaryColor = Color(0xFF789ABC); //not defined yet
+  static const Color dividerColor = Color(0xFFC0C0C0);
+  static const Color grayColor = Color(0xFF9fa7aa);
+  static const Color whiteColor = Colors.white;
+  static const Color blackColor = Colors.black;
+}
