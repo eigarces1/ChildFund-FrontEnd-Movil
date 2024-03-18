bdb71fec02c9bb733d15bc2cca54d991fdf11051
Elian <eigarces1@espe.edu.ec>
2024-03-17 21:03:07 -0500

    respaldooo

---
+++ childfund_evaluation/lib/utils/models/child.dart
@@ -0,0 +1,33 @@
+class Child {
+  final int childId;
+  final int localPartnerId;
+  final int parentId;
+  final String name;
+  final String lastname;
+  final int childNumber;
+  final String gender;
+  final String birthdate;
+  final String community;
+  final String communityType;
+  final String village;
+  final int status;
+  final String updatedAt;
+  final String createdAt;
+
+  Child({
+    required this.childId,
+    required this.localPartnerId,
+    required this.parentId,
+    required this.name,
+    required this.lastname,
+    required this.childNumber,
+    required this.gender,
+    required this.birthdate,
+    required this.community,
+    required this.communityType,
+    required this.village,
+    required this.status,
+    required this.updatedAt,
+    required this.createdAt,
+  });
+}
